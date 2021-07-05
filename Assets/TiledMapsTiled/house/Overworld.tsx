890d8437a5e9ade334c67119f4c05f25726245e0
Daniel Rivas <djra26111990@gmail.com>
2021-07-04 20:46:34 -0500

    [Add] House Room Scene

---
+++ Assets/TiledMapsTiled/house/Overworld.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Overworld" tilewidth="16" tileheight="16" tilecount="1440" columns="40">
+ <image source="../../Art/gfx/Overworld.png" width="640" height="576"/>
+ <terraintypes>
+  <terrain name="camino" tile="0"/>
+ </terraintypes>
+ <tile id="1160" terrain=",,,0"/>
+ <tile id="1161" terrain=",,0,0"/>
+ <tile id="1162" terrain=",,0,"/>
+ <tile id="1200" terrain=",0,,0"/>
+ <tile id="1201" terrain="0,0,0,0"/>
+ <tile id="1202" terrain="0,,0,"/>
+ <tile id="1240" terrain=",0,,"/>
+ <tile id="1241" terrain="0,0,,"/>
+ <tile id="1242" terrain="0,,,"/>
+</tileset>
